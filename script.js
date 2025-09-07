f8e67cf60ff81a8cc4f7a6286e0e4288d39e8b1b
Umar190311 <umarcodingal@gmail.com>
2025-09-07 15:43:28 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 function getHistory(){
     return document.getElementById("history-value").innerText;
 }
-
 function printHistory(num){
     document.getElementById("history-value").innerText=num;
 }
-
 function getOutput(){
     return document.getElementById("output-value").innerText;
 }
-
-function printOutput(){
+function printOutput(num){
     if(num==""){
         document.getElementById("output-value").innerText=num;
     }
@@ -18,7 +15,6 @@ function printOutput(){
         document.getElementById("output-value").innerText=getFormattedNumber(num);
     }
 }
-
 function getFormattedNumber(num){
     if(num=="-"){
         return "";
@@ -27,11 +23,9 @@ function getFormattedNumber(num){
     var value = n.toLocaleString("en");
     return value;
 }
-
 function reverseNumberFormat(num){
     return Number(num.replace(/,/g,''));
 }
-
 var operator = document.getElementsByClassName("operator");
 for(var i =0;i<operator.length;i++){
     operator[i].addEventListener('click',function(){
@@ -39,46 +33,47 @@ for(var i =0;i<operator.length;i++){
             printHistory("");
             printOutput("");
         }
-        else if(this.id="backspace"){
+        else if(this.id=="backspace"){
             var output=reverseNumberFormat(getOutput()).toString();
-            if(output){
-                // if output has avalue
-                output= output.substr(0,output.length-1);
-                printOutput(output);
+            if(output){// if output has a value
+            output= output.substr(0,output.length-1);
+            printOutput(output);    
             }
         }
         else{
-             var output=getOutput();
-             var history=getHistory();
-             if(output==""&&history!=""){
-                history= history.substr(0,history.length-1);
-             }
-        
-        if(output!=="" || history!==""){
-            output= output==""?output:reverseNumberFormat(output);
-            history=history+output;
-            if(this.id=="="){
-                var result=eval(history);
-                printHistory(history);
-                printOutput("");
+            var output=getOutput();
+            var history=getHistory();
+            if(output==""&&history!==""){
+                if(isNaN(history[history.length-1])){
+                    history= history.substr(0,history.length-1);
+                }
             }
-            else{
-                history=history+this.id;
-                printHistory(history);
-                printOutput("");
+            if(output!="" || history!=""){
+                output= output==""?output:reverseNumberFormat(output);
+                history=history+output;
+                if(this.id==""){
+                    var result=eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else{
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
+                } 
             }
-        }
-    }
-});
-}
-var number = document.getElementById("number");
-for(var i =0;i<number.length;i++){
-    number[i].addEventListener('click',function(){
-        var output=reverseNumberFormat(getOutput());
-        if(output!=NaN){ // if output is a number
-            output=output+this.id;
-            printOutput(output);
 
         }
     });
+}
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+    number[i].addEventListener('click',function()
+{
+    var output=reverseNumberFormat(getOutput());
+    if(!isNaN(output)){ // if output is a number
+        output=output+this.id;
+        printOutput(output);
+    }
+});
 }
